f044307184a9faebb2430956e7d97bad849ff652
DevAlePeppermint <xandagropp@hotmail.com>
2021-09-19 10:50:46 -0300

    Configurações iniciais do projeto.

--- psendo/src/App.vue
+++ psendo/src/App.vue
@@ -1,32 +1,28 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app id="inspire">
+    <v-navigation-drawer
+      v-model="drawer"
+      app
+      class="pt-4"
+      color="grey lighten-3"
+      mini-variant
+    >
+      <v-btn elevation="2" style="border-radius: 50%">
+        CLICK
+      </v-btn>
+    </v-navigation-drawer>
+
+    <v-main>
+      <router-view to="/">Home</router-view>
+
+      <!-- Habilitar codigo abaixo quando for implementado router
+      <router-view to="/about">About</router-view> -->
+    </v-main>
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+<script>
+export default {
+  //
+};
+</script>
